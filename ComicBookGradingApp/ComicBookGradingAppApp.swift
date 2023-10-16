8cc90a2ad41c0bee98cd2bd7b7e3f8e05d3287f8
DylanBrass <dylan.brassard@outlook.com>
2023-10-16 09:11:08 -0400

    Initial Commit

---
+++ ComicBookGradingApp/ComicBookGradingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComicBookGradingAppApp.swift
+//  ComicBookGradingApp
+//
+//  Created by macuser on 2023-10-16.
+//
+
+import SwiftUI
+
+@main
+struct ComicBookGradingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComicBookGradingAppUITests/ComicBookGradingAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ComicBookGradingAppUITestsLaunchTests.swift
+//  ComicBookGradingAppUITests
+//
+//  Created by macuser on 2023-10-16.
+//
+
+import XCTest
+
+final class ComicBookGradingAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
