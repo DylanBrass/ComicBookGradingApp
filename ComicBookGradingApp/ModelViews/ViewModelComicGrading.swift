e3e387a6561095e516775fcb577662ded1bf8ec1
DylanBrass <dylan.brassard@outlook.com>
2023-10-18 18:22:02 -0400

    Finished model

---
+++ ComicBookGradingApp/ModelViews/ViewModelComicGrading.swift
@@ -0,0 +1,21 @@
+//
+//  ViewModelComicGrading.swift
+//  ComicBookGradingApp
+//
+//  Created by macuser on 2023-10-18.
+//
+
+import Foundation
+
+
+class ComicGradingViewModel: ObservableObject{
+    
+    @Published var comic: ComicToBeGraded?
+    
+    
+    func startGrading(){
+        
+    }
+    
+    
+}
--- ComicBookGradingApp/Models/ComicGrading.swift
+++ ComicBookGradingApp/Models/ComicGrading.swift
@@ -7,22 +7,47 @@
 
 import Foundation
 
-enum Condition:String{
-    case CVSL
-    case POOR
-    case FR
-    case FR_GD
-    case GD_minus
-    case GD
-}
-
 struct ComicToBeGraded{
     var title: String
     var number: Int?
     var company: String
     var releaseDate: Date
     var marketPriceAtNM: Double?
-    var coverCondition: Condition
+    var coverCondition: [Cover: Condition]
+    var cornerCondition: [Corner: Condition]
+    var damagedPages : [Int: Condition]?
     
-    
+    init(title: String, number: Int? = nil, company: String, releaseDate: Date, marketPriceAtNM: Double? = nil, coverCondition: [Cover : Condition], cornerCondition: [Corner : Condition], damagedPages: [Int : Condition]? = nil) {
+        self.title = title
+        self.number = number
+        self.company = company
+        self.releaseDate = releaseDate
+        self.marketPriceAtNM = marketPriceAtNM
+        self.coverCondition = coverCondition
+        self.cornerCondition = cornerCondition
+        self.damagedPages = damagedPages
+    }
 }
+
+
+enum Condition:Int{
+    case CVSL = 1
+    case POOR = 2
+    case FR = 3
+    case FR_GD = 4
+    case GD_minus = 5
+    case GD = 6
+}
+
+enum Cover:String{
+    case BACK
+    case FRONT
+}
+
+enum Corner: String{
+    case TOP_RIGHT
+    case TOP_LEFT
+    case BOTTOM_RIGHT
+    case BOTTOM_LEFT
+}
+
